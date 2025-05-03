69e537d658a47c7dee959868bf8daaeefe702da4
devjiel <jeremy.leconardel@gmail.com>
2025-05-03 09:58:06 +0200

    move style

--- frontend/src/components/ui/switch.tsx
+++ frontend/src/components/ui/switch.tsx
@@ -1,35 +1,11 @@
 import * as React from "react"
 import * as SwitchPrimitive from "@radix-ui/react-switch"
+import "../../styles/styles.css"
 
-import { cn } from "../../utils/shadcn_utils"
-
-function Switch({
-  className,
-  ...props
-}: React.ComponentProps<typeof SwitchPrimitive.Root>) {
+function Switch(props: React.ComponentProps<typeof SwitchPrimitive.Root>) {
   return (
-    <SwitchPrimitive.Root
-      data-slot="switch"
-      className={cn(
-        "peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors",
-        "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-dark-accent focus-visible:ring-offset-2",
-        "disabled:cursor-not-allowed disabled:opacity-50",
-        "data-[state=unchecked]:bg-dark-secondary",
-        "data-[state=checked]:bg-white",
-        className
-      )}
-      {...props}
-    >
-      <SwitchPrimitive.Thumb
-        data-slot="switch-thumb"
-        className={cn(
-          "pointer-events-none block h-5 w-5 rounded-full shadow-lg ring-0 transition-transform",
-          "data-[state=unchecked]:bg-black",
-          "data-[state=checked]:bg-black",
-          "data-[state=unchecked]:translate-x-0",
-          "data-[state=checked]:translate-x-5"
-        )}
-      />
+    <SwitchPrimitive.Root className="SwitchRoot" {...props}>
+      <SwitchPrimitive.Thumb className="SwitchThumb" />
     </SwitchPrimitive.Root>
   )
 }
